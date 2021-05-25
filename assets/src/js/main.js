1d21827837fbca559e1d1a6ef05b060787656d86
ES-Good <46601455+ES-Good@users.noreply.github.com>
2021-05-25 16:48:48 +0400

    't'

---
+++ assets/src/js/main.js
@@ -0,0 +1,14 @@
+
+
+// Импортируем Popper
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 5
+
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.min.js
+
+
+
+
+// Импортируем другие js-файлы
+//= my.js
